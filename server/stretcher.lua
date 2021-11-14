7c9e777e7d31d2c0961ab58bcf0887b68fe034e8
vanillaa <80897991+evanillaa@users.noreply.github.com>
2021-11-14 08:03:15 +0200

    Update stretcher.lua

--- server/stretcher.lua
+++ server/stretcher.lua
@@ -1,15 +1,12 @@
-RegisterServerEvent('qb-radialmenu:server:RemoveStretcher')
-AddEventHandler('qb-radialmenu:server:RemoveStretcher', function(PlayerPos, StretcherObject)
+RegisterNetEvent('qb-radialmenu:server:RemoveStretcher', function(PlayerPos, StretcherObject)
     TriggerClientEvent('qb-radialmenu:client:RemoveStretcherFromArea', -1, PlayerPos, StretcherObject)
 end)
 
-RegisterServerEvent('qb-radialmenu:Stretcher:BusyCheck')
-AddEventHandler('qb-radialmenu:Stretcher:BusyCheck', function(id, type)
+RegisterNetEvent('qb-radialmenu:Stretcher:BusyCheck', function(id, type)
     local MyId = source
     TriggerClientEvent('qb-radialmenu:Stretcher:client:BusyCheck', id, MyId, type)
 end)
 
-RegisterServerEvent('qb-radialmenu:server:BusyResult')
-AddEventHandler('qb-radialmenu:server:BusyResult', function(IsBusy, OtherId, type)
+RegisterNetEvent('qb-radialmenu:server:BusyResult', function(IsBusy, OtherId, type)
     TriggerClientEvent('qb-radialmenu:client:Result', OtherId, IsBusy, type)
 end)
