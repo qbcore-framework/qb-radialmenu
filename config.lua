2a7c84dd309a844aec6a2993a45d940ebc80a8d3
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-01-11 14:58:42 +0100

    feat(client): Add more security
    This commit does more than add security
    - Optimize PlayerData
    - Add security to selectItem NUI callback
    - Removes Work option if no items are present in it
    - Fixes issue with the Radialmenu opening more than one time when you press F1 causing multiple layers of the Radialmenu to be stuck on your screen
    - Fixed that you couldn't close the Radialmenu with F1
    - Removed useless code

--- config.lua
+++ config.lua
@@ -461,12 +461,6 @@ Config.MenuItems = {
             }
         }
     },
-    [4] = {
-        id = 'jobinteractions',
-        title = 'Work',
-        icon = 'briefcase',
-        items = {}
-    }
 }
 
 Config.JobInteractions = {
@@ -719,34 +713,31 @@ Config.JobInteractions = {
             shouldClose = true
         }
     }
-    -- ["taxi"] = {
-
-    -- },
 }
 
 Config.TrunkClasses = {
-    [0] = {allowed = true, x = 0.0, y = -1.5, z = 0.0}, -- Coupes  
-    [1] = {allowed = true, x = 0.0, y = -2.0, z = 0.0}, -- Sedans  
-    [2] = {allowed = true, x = 0.0, y = -1.0, z = 0.25}, -- SUVs  
-    [3] = {allowed = true, x = 0.0, y = -1.5, z = 0.0}, -- Coupes  
-    [4] = {allowed = true, x = 0.0, y = -2.0, z = 0.0}, -- Muscle  
-    [5] = {allowed = true, x = 0.0, y = -2.0, z = 0.0}, -- Sports Classics  
-    [6] = {allowed = true, x = 0.0, y = -2.0, z = 0.0}, -- Sports  
-    [7] = {allowed = true, x = 0.0, y = -2.0, z = 0.0}, -- Super  
-    [8] = {allowed = false, x = 0.0, y = -1.0, z = 0.25}, -- Motorcycles  
-    [9] = {allowed = true, x = 0.0, y = -1.0, z = 0.25}, -- Off-road  
-    [10] = {allowed = true, x = 0.0, y = -1.0, z = 0.25}, -- Industrial  
-    [11] = {allowed = true, x = 0.0, y = -1.0, z = 0.25}, -- Utility  
-    [12] = {allowed = true, x = 0.0, y = -1.0, z = 0.25}, -- Vans  
-    [13] = {allowed = true, x = 0.0, y = -1.0, z = 0.25}, -- Cycles  
-    [14] = {allowed = true, x = 0.0, y = -1.0, z = 0.25}, -- Boats  
-    [15] = {allowed = true, x = 0.0, y = -1.0, z = 0.25}, -- Helicopters  
-    [16] = {allowed = true, x = 0.0, y = -1.0, z = 0.25}, -- Planes  
-    [17] = {allowed = true, x = 0.0, y = -1.0, z = 0.25}, -- Service  
-    [18] = {allowed = true, x = 0.0, y = -1.0, z = 0.25}, -- Emergency  
-    [19] = {allowed = true, x = 0.0, y = -1.0, z = 0.25}, -- Military  
-    [20] = {allowed = true, x = 0.0, y = -1.0, z = 0.25}, -- Commercial  
-    [21] = {allowed = true, x = 0.0, y = -1.0, z = 0.25} -- Trains  
+    [0] = {allowed = true, x = 0.0, y = -1.5, z = 0.0}, -- Coupes
+    [1] = {allowed = true, x = 0.0, y = -2.0, z = 0.0}, -- Sedans
+    [2] = {allowed = true, x = 0.0, y = -1.0, z = 0.25}, -- SUVs
+    [3] = {allowed = true, x = 0.0, y = -1.5, z = 0.0}, -- Coupes
+    [4] = {allowed = true, x = 0.0, y = -2.0, z = 0.0}, -- Muscle
+    [5] = {allowed = true, x = 0.0, y = -2.0, z = 0.0}, -- Sports Classics
+    [6] = {allowed = true, x = 0.0, y = -2.0, z = 0.0}, -- Sports
+    [7] = {allowed = true, x = 0.0, y = -2.0, z = 0.0}, -- Super
+    [8] = {allowed = false, x = 0.0, y = -1.0, z = 0.25}, -- Motorcycles
+    [9] = {allowed = true, x = 0.0, y = -1.0, z = 0.25}, -- Off-road
+    [10] = {allowed = true, x = 0.0, y = -1.0, z = 0.25}, -- Industrial
+    [11] = {allowed = true, x = 0.0, y = -1.0, z = 0.25}, -- Utility
+    [12] = {allowed = true, x = 0.0, y = -1.0, z = 0.25}, -- Vans
+    [13] = {allowed = true, x = 0.0, y = -1.0, z = 0.25}, -- Cycles
+    [14] = {allowed = true, x = 0.0, y = -1.0, z = 0.25}, -- Boats
+    [15] = {allowed = true, x = 0.0, y = -1.0, z = 0.25}, -- Helicopters
+    [16] = {allowed = true, x = 0.0, y = -1.0, z = 0.25}, -- Planes
+    [17] = {allowed = true, x = 0.0, y = -1.0, z = 0.25}, -- Service
+    [18] = {allowed = true, x = 0.0, y = -1.0, z = 0.25}, -- Emergency
+    [19] = {allowed = true, x = 0.0, y = -1.0, z = 0.25}, -- Military
+    [20] = {allowed = true, x = 0.0, y = -1.0, z = 0.25}, -- Commercial
+    [21] = {allowed = true, x = 0.0, y = -1.0, z = 0.25} -- Trains
 }
 
 Config.ExtrasEnabled = true
