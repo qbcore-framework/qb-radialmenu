fac2565a789b07b13685d4c561d6e8c5e8b78b5b
EinS4ckZwiebeln <66690565+EinS4ckZwiebeln@users.noreply.github.com>
2024-01-10 10:29:16 +0100

    Fix exploitable net events

--- server/stretcher.lua
+++ server/stretcher.lua
@@ -2,10 +2,14 @@ RegisterNetEvent('qb-radialmenu:server:RemoveStretcher', function(pos, stretcher
     TriggerClientEvent('qb-radialmenu:client:RemoveStretcherFromArea', -1, pos, stretcherObject)
 end)
 
-RegisterNetEvent('qb-radialmenu:Stretcher:BusyCheck', function(id, type)
-    TriggerClientEvent('qb-radialmenu:Stretcher:client:BusyCheck', id, source, type)
+RegisterNetEvent('qb-radialmenu:Stretcher:BusyCheck', function(target, type)
+    local src = source
+    if not IsCloseToTarget(src, target) then return end
+    TriggerClientEvent('qb-radialmenu:Stretcher:client:BusyCheck', target, source, type)
 end)
 
-RegisterNetEvent('qb-radialmenu:server:BusyResult', function(isBusy, otherId, type)
-    TriggerClientEvent('qb-radialmenu:client:Result', otherId, isBusy, type)
+RegisterNetEvent('qb-radialmenu:server:BusyResult', function(isBusy, target, type)
+    local src = source
+    if not IsCloseToTarget(src, target) then return end
+    TriggerClientEvent('qb-radialmenu:client:Result', target, isBusy, type)
 end)
